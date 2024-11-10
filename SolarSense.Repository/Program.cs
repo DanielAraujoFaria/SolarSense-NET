c5243420fe8c8337b25dcbdbbc8b07864cc466a7
DanielAraujoFaria <crosscross2.706@gmail.com>
2024-11-10 17:32:01 -0300

    Iniciando Repositório

---
+++ SolarSense.Repository/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SolarSense.Repository
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ SolarSense.Services/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SolarSense.Services
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
