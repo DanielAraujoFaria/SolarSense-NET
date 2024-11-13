100a23356f09f6bd3b7452a206df8ddb2bf2b721
DanielAraujoFaria <crosscross2.706@gmail.com>
2024-11-13 17:56:33 -0300

    Alcançando projeto

--- SolarSense.Repository/Interface/IRepository.cs
+++ SolarSense.Repository/Interface/IRepository.cs
@@ -1,7 +1,12 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
 namespace SolarSense.Repository.Interface
 {
-    internal interface IRepository<T>
+    public interface IRepository<T>
     {
         T GetById(int? id);
         IEnumerable<T> GetAll();
